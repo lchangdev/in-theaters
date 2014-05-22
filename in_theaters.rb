8c284fb0e54a851ccda6a86632229297606b4663
Lawrence Chang <lc.uclakers@gmail.com>
2014-05-22 17:31:00 -0400

    rotten tomatoes api, new sorting function

--- in_theaters.rb
+++ in_theaters.rb
@@ -1,7 +1,20 @@
 require 'json'
 require 'pry'
+require 'json'
+require 'net/http'
 
-movie_data = JSON.parse(File.read('in_theaters.json'))
+if !ENV.has_key?("ROTTEN_TOMATOES_API_KEY")
+  puts "You need to set the ROTTEN_TOMATOES_API_KEY environment variable."
+  exit 1
+end
+
+api_key = ENV["ROTTEN_TOMATOES_API_KEY"]
+uri = URI("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=#{api_key}")
+
+response = Net::HTTP.get(uri)
+movie_data = JSON.parse(response)
+
+# movie_data = JSON.parse(File.read('in_theaters.json'))
 
 puts "In Theaters Now: "
 
@@ -9,33 +22,24 @@ def average(a,b)
   (a + b) / 2
 end
 
-initial_sort = {}
-cast_names1 = []
-cast_names2 = []
-index = 0
+average_rating = {}
 
 movie_data["movies"].each do |x|
 
+  average_score = average(x["ratings"]["critics_score"], x["ratings"]["audience_score"])
+
   if x["abridged_cast"][1] == nil
-    cast_names1 << "none"
-  elsif x["abridged_cast"][1] != nil
-    cast_names1 << x["abridged_cast"][1]["name"]
+    average_rating[average_score] = "#{x["title"]}, #{x["mpaa_rating"]}, #{x["abridged_cast"][0]["name"]}"
+  else
+    average_rating[average_score] = "#{x["title"]}, #{x["mpaa_rating"]}, #{x["abridged_cast"][0]["name"]}, #{x["abridged_cast"][1]["name"]}, #{x["abridged_cast"][2]["name"]}"
   end
-
-  if x["abridged_cast"][2] == nil
-    cast_names2 << "none"
-  elsif x["abridged_cast"][2] != nil
-    cast_names2 << x["abridged_cast"][2]["name"]
-  end
-
-  initial_sort[average(x["ratings"]["critics_score"], x["ratings"]["audience_score"])] = [x["title"], x["mpaa_rating"], x["abridged_cast"][0]["name"], cast_names1[index], cast_names2[index]]
 
 end
 
-sorted_movies = initial_sort.sort_by {|k,v| k}.reverse
+average_rating = average_rating.sort_by {|k,v| k}.reverse
 
-Hash[sorted_movies].each do |k,v|
-  puts "#{k} - #{v.join(", ")}"
+Hash[average_rating].each do |k,v|
+  puts "#{k} - #{v}"
 end
 
 # ==================================================
