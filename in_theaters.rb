d1ae60c4ecd48d02ae0bbd41d5fc546a97e90172
Lawrence Chang <lc.uclakers@gmail.com>
2014-05-22 17:02:50 -0400

    finished sort print out

--- in_theaters.rb
+++ in_theaters.rb
@@ -3,35 +3,13 @@ require 'pry'
 
 movie_data = JSON.parse(File.read('in_theaters.json'))
 
-# list of movies in theatres now FINISHED
-
-# puts "In Theaters Now: "
-# movie_data["movies"].each do |x|
-#   puts x["title"]
-# end
-
-# MPAA rating along w/ each movies FINISHED
-
-# puts "In Theaters Now: "
-
-# movie_data["movies"].each do |x|
-#   puts "#{x["title"]} (#{x["mpaa_rating"]}) "
-# end
-
-
-# Average of the critics & audience scores FINISHED
-
-# puts "In Theaters Now: "
-
-# movie_data["movies"].each do |x|
-#   puts "#{(x["ratings"]["critics_score"] + x["ratings"]["audience_score"]) / 2 } - \
-# #{x["title"]} (#{x["mpaa_rating"]}) "
-# end
-
-# Up to the first three cast members FINISHED
-
 puts "In Theaters Now: "
 
+def average(a,b)
+  (a + b) / 2
+end
+
+initial_sort = {}
 cast_names1 = []
 cast_names2 = []
 index = 0
@@ -50,44 +28,75 @@ movie_data["movies"].each do |x|
     cast_names2 << x["abridged_cast"][2]["name"]
   end
 
-  puts "#{(x["ratings"]["critics_score"] + x["ratings"]["audience_score"]) / 2 } - \
-#{x["title"]} (#{x["mpaa_rating"]}) starring #{x["abridged_cast"][0]["name"]}, \
-#{cast_names1[index]}, #{cast_names2[index]}"
+  initial_sort[average(x["ratings"]["critics_score"], x["ratings"]["audience_score"])] = [x["title"], x["mpaa_rating"], x["abridged_cast"][0]["name"], cast_names1[index], cast_names2[index]]
 
-  index += 1
 end
 
+sorted_movies = initial_sort.sort_by {|k,v| k}.reverse
 
+Hash[sorted_movies].each do |k,v|
+  puts "#{k} - #{v.join(", ")}"
+end
 
+# ==================================================
+# list of movies in theatres now
 
+# puts "In Theaters Now: "
+# movie_data["movies"].each do |x|
+#   puts x["title"]
+# end
 
-
-
-# Sorted by highest rating first. ON GOING
+# MPAA rating along w/ each movies FINISHED
 
 # puts "In Theaters Now: "
 
-
-# def average(a,b)
-#   (a + b) / 2
+# movie_data["movies"].each do |x|
+#   puts "#{x["title"]} (#{x["mpaa_rating"]}) "
 # end
-# initial_sort = {}
-# casts_names = []
+
+# ==================================================
+
+# Average of the critics & audience scores
+
+# puts "In Theaters Now: "
+
+# movie_data["movies"].each do |x|
+#   puts "#{(x["ratings"]["critics_score"] + x["ratings"]["audience_score"]) / 2 } - \
+# #{x["title"]} (#{x["mpaa_rating"]}) "
+# end
+
+# ==================================================
+
+# Up to the first three cast members
+
+# puts "In Theaters Now: "
+
+# cast_names1 = []
+# cast_names2 = []
+# index = 0
+
 # movie_data["movies"].each do |x|
 
-# # some arrays are outputing nil so have issue
+#   if x["abridged_cast"][1] == nil
+#     cast_names1 << "none"
+#   elsif x["abridged_cast"][1] != nil
+#     cast_names1 << x["abridged_cast"][1]["name"]
+#   end
 
-#  puts x["abridged_cast"][1]["name"]
+#   if x["abridged_cast"][2] == nil
+#     cast_names2 << "none"
+#   elsif x["abridged_cast"][2] != nil
+#     cast_names2 << x["abridged_cast"][2]["name"]
+#   end
 
+#   puts "#{(x["ratings"]["critics_score"] + x["ratings"]["audience_score"]) / 2 } - \
+# #{x["title"]} (#{x["mpaa_rating"]}) starring #{x["abridged_cast"][0]["name"]}, \
+# #{cast_names1[index]}, #{cast_names2[index]}"
 
-#   # initial_sort[average(x["ratings"]["critics_score"], x["ratings"]["audience_score"])] = [x["title"], x["mpaa_rating"], x["abridged_cast"][0]["name"]]
-# #  #{x["title"]} (#{x["mpaa_rating"]}) starring #{x["abridged_cast"][0]["name"]}, \
-# #  #{x["abridged_cast"][1]["name"]}, #{x["abridged_cast"][2]["name"]}"
+#   index += 1
 # end
 
-# puts casts_names
-# puts initial_sort
-
+# ==================================================
 
 
 
